11b906ad33fb267dbcbe7edb1c236d04f0cac019
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-09 06:37:37 -0700

    Update and Inster added to User Data Query

--- TCB.G1Group/DataService/UserDataQuerys.cs
+++ TCB.G1Group/DataService/UserDataQuerys.cs
@@ -11,5 +11,7 @@ public static class UserDataQuerys
     private static string selectByIdQuery = $"SELECT * FROM {tableName} WHERE id = @p0";
     private static string selectByChatIdQuery = $"SELECT * FROM {tableName} WHERE telegram_client_id= @p0";
     private static string selectByLoginAndPassword = $"SELECT * FROM {tableName} WHERE phone_number = @p0 and password = @p1;";
-     
+    private static string insertQuery =
+        $"INSERT INTO {tableName} (telegram_client_id,phone_number,password) VALUES ( @p1, @p2, @p3) RETURNING *;";
+    private static string updateQuery = $"UPDATE {tableName} SET (phone_number,password) VALUES (@p1,@p2) RETURNING *;";
 }
