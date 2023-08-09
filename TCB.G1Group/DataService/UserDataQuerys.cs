bf384da12770f6d8b60ad4d714c76ef12bcd1e2f
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-09 06:25:02 -0700

    Data Querys for User

---
+++ TCB.G1Group/DataService/UserDataQuerys.cs
@@ -0,0 +1,15 @@
+using TCB.G1Group.DataService.Interface;
+using TCB.G1Group.Domain.Modles;
+
+namespace TCB.G1Group.DataService;
+
+public static class UserDataQuerys
+{
+    private static string tableName = "users";
+
+    private static string selectQuery = $"SELECT * FROM {tableName}";
+    private static string selectByIdQuery = $"SELECT * FROM {tableName} WHERE id = @p0";
+    private static string selectByChatIdQuery = $"SELECT * FROM {tableName} WHERE telegram_client_id= @p0";
+    private static string selectByLoginAndPassword = $"SELECT * FROM {tableName} WHERE phone_number = @p0 and password = @p1;";
+    
+}
