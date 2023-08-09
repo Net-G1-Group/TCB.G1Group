c72325129526483f5751d1855da8c2a569e01293
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-09 18:38:53 +0500

    creaded messageDAtaService and ignire file .idea

---
+++ TCB.G1Group/DataService/MessageDataService.cs
@@ -0,0 +1,32 @@
+﻿using TCB.G1Group.DataService.Interface;
+using TCB.G1Group.Domain.Models;
+
+namespace TCB.G1Group.DataService;
+
+public class MessageDataService : IMessageDataService
+{
+    public async Task<Message> Create(Message data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Message> UpDate(Message data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Message> Delete(Message data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Message>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Message> FindById(long Id)
+    {
+        throw new NotImplementedException();
+    }
+}
