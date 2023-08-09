e1116e3a556f8a12f0c6ed260b9596477769234e
mamat <mamatov.nodir0506@mail.com>
2023-08-09 17:50:31 +0500

    Description created

--- TCB.G1Group/Domain/Modles/Message.cs
+++ TCB.G1Group/Domain/Modles/Message.cs
@@ -1,15 +1,18 @@
+using System.ComponentModel;
 using TCB.G1Group.Domain.Enums;
 
 namespace TCB.G1Group.Domain.Modles;
 
 public class Message
 {
+    [Description("from_id")]
     public long FromId { get; set; }
+    [Description("message")]
     public object _Message { get; set; }
+    [Description("chat_id")]
     public long ChatId { get; set; }
+    [Description("type")]
     public MessageType Type { get; set; }
+    [Description("board_id")]
     public long BoardId { get; set; }
-    
-    
-    
 }
