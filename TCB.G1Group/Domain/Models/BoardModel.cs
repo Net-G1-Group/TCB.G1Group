35e13bfada31f126e6bdcc5027051e829850e819
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-09 17:59:39 +0500

    edit namespase

---
+++ TCB.G1Group/Domain/Models/BoardModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using TCB.G1Group.Domain.Enums;
+
+namespace TCB.G1Group.Domain.Models;
+
+public class BoardModel : ModelBase
+{
+    [Description("nickname")] public string NickName { get; set; }
+
+    [Description("owner_id")] public long OwnerId { get; set; }
+
+    [Description("board_status")] public BoardStatus BoardStatus { get; set; }
+
+    public List<Message> MessageList { get; set; }
+}
--- TCB.G1Group/Domain/Models/Client.cs
+++ TCB.G1Group/Domain/Models/Client.cs
@@ -1,20 +1,14 @@
 using System.ComponentModel;
 using TCB.G1Group.Domain.Enums;
 
-namespace TCB.G1Group.Domain.Modles;
+namespace TCB.G1Group.Domain.Models;
 
 public class Client : ModelBase
 {
-    [Description("user_id")]
-    public long UserId { get; set; }
-    [Description("chat_id")]
-    public long TelegramChatId { get; set; }
-    [Description("nick_name")]
-    public string? NickName { get; set; }
-    [Description("is_premium")]
-    public bool IsPremium { get; set; }
-    [Description("status")]
-    public ClientStatus Status { get; set; }
-    [Description("client_in_anonymChat")]
-    public bool ClientInAnonymChat { get; set; } = false;
+    [Description("user_id")] public long UserId { get; set; }
+    [Description("chat_id")] public long TelegramChatId { get; set; }
+    [Description("nick_name")] public string? NickName { get; set; }
+    [Description("is_premium")] public bool IsPremium { get; set; }
+    [Description("status")] public ClientStatus Status { get; set; }
+    [Description("client_in_anonymChat")] public bool ClientInAnonymChat { get; set; } = false;
 }
