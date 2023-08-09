2b5c804368f6cd780057aade64684a6fe4e99135
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-09 09:36:31 -0700

    Bug fixed

---
+++ TCB.G1Group/AuthService.cs
@@ -0,0 +1,48 @@
+using TCB.G1Group.DataService;
+using TCB.G1Group.Domain.Enums;
+using TCB.G1Group.Domain.Models;
+using TCB.G1Group.Domain.Modles;
+
+namespace TCB.G1Group;
+
+public class AuthService
+{
+    private UserDataService _userDataService;
+    private ClientDataService _clientDataService;
+    public AuthService(UserDataService userDataService,ClientDataService clientDataService)
+    {
+        _userDataService = userDataService;
+        _clientDataService = clientDataService;
+    }
+    public async Task RegisterUser(User userRegistration)
+    {
+        var User = await _userDataService.Create(new User()
+        {
+            Password = userRegistration.Password,
+            PhoneNumber = userRegistration.PhoneNumber,
+            TelegramClientId = userRegistration.TelegramClientId
+        });
+        if (User is null)
+            throw new Exception("Unable to insert user");
+        await _clientDataService.Create(new Client()
+        {
+            UserId = User.Id,
+            Status = ClientStatus.Enabled,
+            NickName = string.Empty,
+          
+            IsPremium = false,
+        });
+    }
+
+
+    public async Task<Client> Login(User user)
+    {
+        var userInfo = await _userDataService.FindPhoneAndPassword(user.PhoneNumber, user.Password);
+
+        if (userInfo is not null)
+        {
+            return await _clientDataService.FindByUserId(userInfo.Id);
+        }
+        return null;
+    }
+}
