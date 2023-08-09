87c21383e0b141a53d24285894867354ebb8caa2
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-09 17:23:42 +0500

    creaded Anonym chat class

---
+++ TCB.G1Group/Domain/Enums/ChatState.cs
@@ -0,0 +1,9 @@
+﻿namespace TCB.G1Group.Domain.Enums;
+
+public enum ChatState
+{
+    New,
+    Opened,
+    Closed,
+    Aborted
+}
---
+++ TCB.G1Group/Domain/Modles/AnonymChat.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using TCB.G1Group.Domain.Enums;
+
+namespace TCB.G1Group.Domain.Modles;
+
+public class AnonymChat : ModelBase
+{
+    [Description("create_date")] public DateTime CreatedDate { get; set; }
+    [Description("from_id")] public long FromId { get; set; }
+    [Description("to_id")] public long ToId { get; set; }
+    [Description("state")] public ChatState State { get; set; }
+}
