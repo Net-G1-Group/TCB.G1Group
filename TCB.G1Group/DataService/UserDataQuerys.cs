0717ea9fe2fb7d7aefd5380e69853c3e6aa83271
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-09 06:39:53 -0700

    Bug fixed

--- TCB.G1Group/DataService/UserDataQuerys.cs
+++ TCB.G1Group/DataService/UserDataQuerys.cs
@@ -7,11 +7,11 @@ public static class UserDataQuerys
 {
     private static string tableName = "users";
 
-    private static string selectQuery = $"SELECT * FROM {tableName}";
-    private static string selectByIdQuery = $"SELECT * FROM {tableName} WHERE id = @p0";
-    private static string selectByChatIdQuery = $"SELECT * FROM {tableName} WHERE telegram_client_id= @p0";
-    private static string selectByLoginAndPassword = $"SELECT * FROM {tableName} WHERE phone_number = @p0 and password = @p1;";
-    private static string insertQuery =
+    public static string selectQuery = $"SELECT * FROM {tableName}";
+    public static string selectByIdQuery = $"SELECT * FROM {tableName} WHERE id = @p0";
+    public static string selectByChatIdQuery = $"SELECT * FROM {tableName} WHERE telegram_client_id= @p0";
+    public static string selectByLoginAndPassword = $"SELECT * FROM {tableName} WHERE phone_number = @p0 and password = @p1;";
+    public static string insertQuery =
         $"INSERT INTO {tableName} (telegram_client_id,phone_number,password) VALUES ( @p1, @p2, @p3) RETURNING *;";
     private static string updateQuery = $"UPDATE {tableName} SET (phone_number,password) VALUES (@p1,@p2) RETURNING *;";
 }
