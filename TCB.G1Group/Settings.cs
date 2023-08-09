6c340f7d50c90e5581a5bdefea0be27b4f5b2e75
AbduRohim2004 <abdurohimbaxtiyorov@gmail.com>
2023-08-09 18:30:09 +0500

    Settings is changed

--- TCB.G1Group/Settings.cs
+++ TCB.G1Group/Settings.cs
@@ -2,6 +2,6 @@ namespace TCB.G1Group;
 
 public static class Settings
 {
-    public static string TelegramBotToken { get; set; } = "";
-    public static string com  { get; set; } = "Host=localhost; Port=5432; Database=null; username=postgres; password=null";
+    public static string TelegramBotToken = "";
+    public static string com = "Host=localhost; Port=5432; Database=null; username=postgres; password=null";
 }
