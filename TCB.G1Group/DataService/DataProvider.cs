94f4535b24264d7f0913e88df3b5d31dc3cdd887
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-09 18:11:07 +0500

    created data provider and nuGet pakect installer

---
+++ TCB.G1Group/DataService/DataProvider.cs
@@ -0,0 +1,46 @@
+﻿using Npgsql;
+
+namespace TCB.G1Group.DataService;
+
+public class DataProvider
+{
+    private readonly string _connectionString;
+
+    public DataProvider(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public NpgsqlConnection CreateNewConnection()
+    {
+        return new NpgsqlConnection(this._connectionString);
+    }
+
+    public async Task<NpgsqlDataReader>ExecuteWithResult(string query, NpgsqlParameter[]? parameters)
+    {
+        var connection = this.CreateNewConnection();
+        await connection.OpenAsync();
+
+        var command = new NpgsqlCommand(query, connection);
+        if (parameters is not null)
+            command.Parameters.AddRange(parameters);
+
+        var reader = await command.ExecuteReaderAsync();
+
+        return reader;
+    }
+    
+    public async Task<int> ExecuteNonResult(string query, NpgsqlParameter[]? parameters)
+    {
+        var connection = this.CreateNewConnection();
+        await connection.OpenAsync();
+
+        var command = new NpgsqlCommand(query, connection);
+        if (parameters is not null)
+            command.Parameters.AddRange(parameters);
+
+        var result = await command.ExecuteNonQueryAsync();
+
+        return result;
+    }
+}
