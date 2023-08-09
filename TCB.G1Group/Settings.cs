49743488598222240e0222ccd3511c5e2db2a8d0
AbduRohim2004 <abdurohimbaxtiyorov@gmail.com>
2023-08-09 18:26:01 +0500

    Settings class is added

---
+++ TCB.G1Group/Settings.cs
@@ -0,0 +1,7 @@
+namespace TCB.G1Group;
+
+public static class Settings
+{
+    public static string TelegramBotToken { get; set; } = "";
+    public static string com  { get; set; } = "Host=localhost; Port=5432; Database=null; username=postgres; password=null";
+}
