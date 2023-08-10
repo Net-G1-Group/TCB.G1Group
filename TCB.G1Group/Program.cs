0fec419c283a3de317a3aa4cb6998859705a4996
Azimjon <azimabdishukurov@gmail.com>
2023-08-10 14:23:56 +0500

    Interface Service is added

--- TCB.G1Group/Program.cs
+++ TCB.G1Group/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TCB.G1Group.Confugration;
+using TCB.G1Group.DataService;
+using TCB.G1Group.Domain.Enums;
+using TCB.G1Group.Domain.Models;
 
-Console.WriteLine("Hello, World!");
+BoardModel boardModel = new BoardModel
+{
+    Id = 0,
+    NickName = "Elnur1211",
+    OwnerId = 0,
+    BoardStatus = BoardStatus.Processing,
+    MessageList = new List<Message>()
+};
+MessageDataService message = new TCB.G1Group.DataService.MessageDataService(Settings.com);
+BoardDataService v = new BoardDataService(Settings.com,message);
+var a = v.Create(boardModel);
+var s = v.Create(boardModel);
+var b = v.Update(1,boardModel);
+//
+var kk = v.BoardMessages(1);
+var qq = v.BoardMessageState(1);
+var k = v.Delete(1);
+Console.WriteLine();
